4bb43deedb8125bfcfca886550b77a809ef03f02
dorbae <dorbae.io@gmail.com>
2025-08-14 01:38:42 +0900

    Apply composer

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 import { ChevronUpIcon, EyeIcon, MessageCircleIcon } from "lucide-react";
 import { Button } from "../components/ui/button";
+import ProductCard from "~/features/products/components/product-card";
 
 export function loader({ request }: Route.LoaderArgs) {
   return {
@@ -30,35 +31,16 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
           <h2 className="text-5xl font-bold leading-tight tracking-tight">Today's Products</h2>
           <p className="text-xl font-light text-foreground">The best products made by our comunity today.</p>
         </div>
-        <div>
-          <Link to={`/products/productId`}>
-            <Card className="w-full flex items-center justify-between bg-transparent hover:bg-card/50">
-              <CardHeader>
-                <CardTitle className="text-2xl font-semibold leading-none tracking-tight">
-                  Product Name
-                </CardTitle>
-                <CardDescription className="text-sm text-muted-foreground">
-                  Product Description
-                </CardDescription>
-                <div className="flex items-center gap-4 mt-2">
-                  <div className="flex items-center gap-px text-xs text-muted-foreground">
-                    <MessageCircleIcon className="size-4" />
-                    <span>12</span>
-                  </div>
-                  <div className="flex items-center gap-1">
-                    <EyeIcon className="size-4" />
-                    <span>12</span>
-                  </div>
-                </div>
-              </CardHeader>
-              <CardFooter className="py-0">
-                <Button variant="outline" className="flex items-center h-14 gap-1">
-                  <ChevronUpIcon className="size-4 shrink-0" />
-                  <span className="text-xs font-semibold">120</span>
-                </Button>
-              </CardFooter>
-            </Card></Link>
-        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`product-${index}`}
+            name={`Product ${index + 1}`}
+            description={`This is the ${index + 1}th product description`}
+            commentsCount={12}
+            viewsCount={156}
+          />
+        ))}
       </div>
     </div>
   );
