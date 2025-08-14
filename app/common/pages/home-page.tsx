ae35d5434760a499cab7d824ae4802ecd1f7ea23
dorbae <dorbae.io@gmail.com>
2025-08-14 22:09:09 +0900

    Add post card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -4,6 +4,7 @@ import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "../com
 import { ChevronUpIcon, EyeIcon, MessageCircleIcon } from "lucide-react";
 import { Button } from "../components/ui/button";
 import ProductCard from "~/features/products/components/product-card";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 
 export function loader({ request }: Route.LoaderArgs) {
   return {
@@ -25,7 +26,7 @@ export function meta(): Route.MetaFunction {
 
 export default function HomePage({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">Today's Products</h2>
@@ -34,7 +35,7 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
             <Link to="/products/leaderboards">Explore all products &rarr;</Link>
           </Button>
         </div>
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: 5 }).map((_, index) => (
           <ProductCard
             key={index}
             id={`product-${index}`}
@@ -44,6 +45,38 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
             viewsCount={156}
           />
         ))}
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Latest discussions</h2>
+          <p className="text-xl font-light text-foreground">The latest discussions made by our comunity today.</p>
+          <Button variant="link" className="p-0" asChild>
+            <Link to="/products/leaderboards">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        <Card className="bg-transparent hover:bg-card/50 transition-colors">
+          <CardHeader className="flex flex-row items-center gap-2">
+            <Avatar className="size-14">
+              <AvatarFallback>N</AvatarFallback>
+              <AvatarImage src="https://github.com/apple.png" />
+            </Avatar>
+            <div className="space-y-2">
+              <CardTitle className="text-2xl font-semibold leading-none tracking-tight">Discussion Title</CardTitle>
+              <div className="flex items-center gap-2 text-xs leading-tight text-muted-foreground">
+                <span>Nico</span>
+                <span>•</span>
+                <span>Productivity</span>
+                <span>•</span>
+                <span>12 hours ago</span>
+              </div>
+            </div>
+          </CardHeader>
+          <CardFooter className="flex justify-end">
+            <Button variant="link" asChild>
+              <Link to="/community/postId">Reply &rarr;</Link>
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
