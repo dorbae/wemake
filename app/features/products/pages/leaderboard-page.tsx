e6d66fdcb89a1fff5f101b799773c210beffb88f
dorbae <dorbae.io@gmail.com>
2025-08-19 14:24:55 +0900

    Add daily/weekly/monthly/yearly cards

--- app/features/products/pages/leaderboard-page.tsx
+++ app/features/products/pages/leaderboard-page.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "../../../+types/routes";
-import type { MetaFunction } from "react-router";
+import { Link, type MetaFunction } from "react-router";
 import { HeroSection } from "~/common/components/hero-section";
+import { ProductCard } from "../components/product-card";
+import { Button } from "~/common/components/ui/button";
 
 export function loader({ request }: Route.LoaderArgs) {
   return {};
@@ -22,6 +24,86 @@ export default function LeaderboardPage({ loaderData }: Route.ComponentProps) {
         size="lg"
         variant="gradient"
       />
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Daily Leaderboard</h2>
+          <p className="text-xl font-light text-foreground">The most popular products on wake by day.</p>
+        </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`product-${index}`}
+            name={`Product ${index + 1}`}
+            description={`This is the ${index + 1}th product description`}
+            commentsCount={12}
+            viewsCount={156}
+            votesCount={12}
+          />
+        ))}
+        <Button variant="link" className="p-0 self-center" asChild>
+          <Link to="/products/leaderboards/daily">Explore all products &rarr;</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Weekly Leaderboard</h2>
+          <p className="text-xl font-light text-foreground">The most popular products on wake by week.</p>
+        </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`product-${index}`}
+            name={`Product ${index + 1}`}
+            description={`This is the ${index + 1}th product description`}
+            commentsCount={12}
+            viewsCount={156}
+            votesCount={12}
+          />
+        ))}
+        <Button variant="link" className="p-0 self-center" asChild>
+          <Link to="/products/leaderboards/weekly">Explore all products &rarr;</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Monthly Leaderboard</h2>
+          <p className="text-xl font-light text-foreground">The most popular products on wake by month.</p>
+        </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`product-${index}`}
+            name={`Product ${index + 1}`}
+            description={`This is the ${index + 1}th product description`}
+            commentsCount={12}
+            viewsCount={156}
+            votesCount={12}
+          />
+        ))}
+        <Button variant="link" className="p-0 self-center" asChild>
+          <Link to="/products/leaderboards/monthly">Explore all products &rarr;</Link>
+        </Button>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Yearly Leaderboard</h2>
+          <p className="text-xl font-light text-foreground">The most popular products on wake by year.</p>
+        </div>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`product-${index}`}
+            name={`Product ${index + 1}`}
+            description={`This is the ${index + 1}th product description`}
+            commentsCount={12}
+            viewsCount={156}
+            votesCount={12}
+          />
+        ))}
+        <Button variant="link" className="p-0 self-center" asChild>
+          <Link to="/products/leaderboards/yearly">Explore all products &rarr;</Link>
+        </Button>
+      </div>
     </div>
   );
 }
