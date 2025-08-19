ae113a532bbfbf5efce88af13c8b161ac2209247
dorbae <dorbae.io@gmail.com>
2025-08-19 14:07:46 +0900

    Apply component by composer

--- app/features/products/pages/leaderboard-page.tsx
+++ app/features/products/pages/leaderboard-page.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "../../../+types/routes";
 import type { MetaFunction } from "react-router";
+import { HeroSection } from "~/common/components/hero-section";
 
 export function loader({ request }: Route.LoaderArgs) {
   return {};
@@ -15,12 +16,12 @@ export const meta: MetaFunction = () => {
 export default function LeaderboardPage({ loaderData }: Route.ComponentProps) {
   return (
     <div>
-      <div className="flex flex-col py-20 justify-center items-center rounded-md bg-gradient-to-b from-background via-primary/10 to-primary/20">
-        <h1 className="text-5xl font-bold">Leaderboard</h1>
-        <p className="text-2xl font-light text-foreground">
-          Top most popular products on wemake
-        </p>
-      </div>
+      <HeroSection
+        title="Leaderboard"
+        description="Top most popular products on wemake"
+        size="lg"
+        variant="gradient"
+      />
     </div>
   );
 }
