668f6c92386b17ca12ef1ce1b2c95626632bd09a
dorbae <dorbae.io@gmail.com>
2025-08-19 14:01:53 +0900

    Fix not visible gradient

--- app/features/products/pages/leaderboard-page.tsx
+++ app/features/products/pages/leaderboard-page.tsx
@@ -1,7 +1,6 @@
 import type { Route } from "../../../+types/routes";
 import type { MetaFunction } from "react-router";
 
-
 export function loader({ request }: Route.LoaderArgs) {
   return {};
 }
@@ -16,12 +15,12 @@ export const meta: MetaFunction = () => {
 export default function LeaderboardPage({ loaderData }: Route.ComponentProps) {
   return (
     <div>
-      <div className="flex flex-col py-20 justify-center items-center rounded-md bg-gradient-to-t from-background to-primary-10">
+      <div className="flex flex-col py-20 justify-center items-center rounded-md bg-gradient-to-b from-background via-primary/10 to-primary/20">
         <h1 className="text-5xl font-bold">Leaderboard</h1>
         <p className="text-2xl font-light text-foreground">
-          Top most poluar products on wemake
+          Top most popular products on wemake
         </p>
       </div>
     </div>
-  )
+  );
 }
