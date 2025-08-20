30b9f25b01c91f81e190f4652224380d43d18481
dorbae <dorbae.io@gmail.com>
2025-08-20 21:59:59 +0900

    Specify error case

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,13 +1,21 @@
 import { DateTime } from "luxon";
 import type { Route } from "../../../+types/root";
-import type { MetaFunction } from "react-router";
+import { data, isRouteErrorResponse, type MetaFunction } from "react-router";
 
 export function loader({ request, params }: Route.LoaderArgs) {
   const { year, month, day } = params;
   const date = DateTime.fromObject({ year: Number(year), month: Number(month), day: Number(day) });
   if (!date.isValid) {
     // @NOTICE: Occurs error boundary
-    throw new Error("Invalid date");
+    throw data (
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      {
+        status: 400,
+      }
+    );
   }
 
   return { year, month, day };
@@ -26,5 +34,22 @@ export default function DailyLeaderboardPage({ loaderData }: Route.ComponentProp
 
 // @NOTICE: Overriding root error boundary
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  return <div>Daily Leaderboard Error</div>;
+  // @NOTICE: If status is 4xx or 5xx
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    )
+  }
+
+  if (error instanceof Error) {
+    return (
+      <div>
+        {error.message}
+      </div>
+    )
+  }
+
+  return <div>Unknown error</div>;
 }
