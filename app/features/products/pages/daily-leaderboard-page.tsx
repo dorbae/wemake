69413303bde97b72d7abbc1ae58075604bea689e
dorbae <dorbae.io@gmail.com>
2025-08-20 22:34:58 +0900

    Enhance validation using zod

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,13 +1,19 @@
 import { DateTime } from "luxon";
 import type { Route } from "../../../+types/root";
 import { data, isRouteErrorResponse, type MetaFunction } from "react-router";
+import { z } from "zod";
+
+const paramSchema = z.object({
+  year: z.coerce.number().int().min(1900).max(2100),
+  month: z.coerce.number().int().min(1).max(12),
+  day: z.coerce.number().int().min(1).max(31),
+});
 
 export function loader({ request, params }: Route.LoaderArgs) {
-  const { year, month, day } = params;
-  const date = DateTime.fromObject({ year: Number(year), month: Number(month), day: Number(day) });
-  if (!date.isValid) {
-    // @NOTICE: Occurs error boundary
-    throw data (
+  const { success, data: parsedParams } = paramSchema.safeParse(params);
+  // @NOTICE: Occurs error boundary
+  if (!success) {
+    throw data(
       {
         error_code: "invalid_date",
         message: "Invalid date",
@@ -18,7 +24,20 @@ export function loader({ request, params }: Route.LoaderArgs) {
     );
   }
 
-  return { year, month, day };
+  const date = DateTime.fromObject(parsedParams);
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today) {
+    throw data(
+      {
+        error_code: "invalid_date",
+        message: "Invalid date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  return { date };
 }
 
 export const meta: MetaFunction = () => {
@@ -29,8 +48,9 @@ export const meta: MetaFunction = () => {
 };
 
 export default function DailyLeaderboardPage({ loaderData }: Route.ComponentProps) {
+  console.log("aa");
   return;
-} 
+}
 
 // @NOTICE: Overriding root error boundary
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
