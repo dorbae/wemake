941be103f5c19f5380415116110f059ff9daeee9
dorbae <dorbae.io@gmail.com>
2025-08-20 14:27:17 +0900

    Add vaildation for date

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,8 +1,15 @@
+import { DateTime } from "luxon";
 import type { Route } from "../../../+types/routes";
 import type { MetaFunction } from "react-router";
 
 export function loader({ request, params }: Route.LoaderArgs) {
   const { year, month, day } = params;
+  const date = DateTime.fromObject({ year: Number(year), month: Number(month), day: Number(day) });
+  if (!date.isValid) {
+    // @NOTICE: Occurs error boundary
+    throw new Error("Invalid date");
+  }
+  
   return { year, month, day };
 }
 
@@ -15,4 +22,9 @@ export const meta: MetaFunction = () => {
 
 export default function DailyLeaderboardPage({ loaderData }: Route.ComponentProps) {  
   return;
+}
+
+// @NOTICE: Overriding root error boundary
+export function ErrorBoundary({ error }: { error: Error }) {
+  return <div>Daily Leaderboard Error: {error.message}</div>;
 }
