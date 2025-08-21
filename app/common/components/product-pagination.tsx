19d0ad2caecd8a453446614e661be23ff8f8928d
dorbae <dorbae.io@gmail.com>
2025-08-21 23:21:05 +0900

    Add mock pagination

--- app/common/components/product-pagination.tsx
+++ app/common/components/product-pagination.tsx
@@ -1,8 +1,29 @@
+import { Pagination, PaginationEllipsis, PaginationLink, PaginationPrevious, PaginationContent, PaginationItem, PaginationNext } from "./ui/pagination";
+
 type ProductPaginationProps = {
     totalPages: number;
 }
 
 
 export default function ProductPagination({ totalPages }: ProductPaginationProps) {
-    return <div>ProductPagination</div>
+    return (
+        <div>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </div>
+    )
 } 
