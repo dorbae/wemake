5c86b565baa2db07c1886f032ccbaa583b8635bd
dorbae <dorbae.io@gmail.com>
2025-08-28 22:54:36 +0900

    Configure drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+// @NOTICE: Referenced by Dizzle Kit
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/features/***/schema.ts",
+  out: "./app/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
